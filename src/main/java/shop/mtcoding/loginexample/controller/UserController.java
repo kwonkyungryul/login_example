7f8402d599ea2470f2b4348ca1921f9c428205bf
kkr <kkr0787@gmail.com>
2023-02-21 14:08:38 +0900

    메인 뷰 생성 완료

--- src/main/java/shop/mtcoding/loginexample/controller/UserController.java
+++ src/main/java/shop/mtcoding/loginexample/controller/UserController.java
@@ -3,6 +3,7 @@ package shop.mtcoding.loginexample.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,16 @@ public class UserController {
 
     @Autowired
     HttpSession session;
+
+    @GetMapping("/")
+    public String mainPage() {
+        User principal = (User) session.getAttribute("principal");
+        if (principal == null) {
+            throw new CustomException("잘못된 접근입니다. 로그인을 해주세요.", HttpStatus.UNAUTHORIZED);
+        }
+        
+        return "main";
+    }
 
     @GetMapping("/loginForm")
     public String loginForm() {
